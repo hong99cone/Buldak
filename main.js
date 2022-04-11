e175173f96b645b43c11fe42c0accdbc939502c9
HongJu <03mm007mm@gmail.com>
2022-04-12 03:20:11 +0900

    test

--- main.js
+++ main.js
@@ -311,29 +311,28 @@ var mapContainer = document.getElementById('map'), // 지도를 표시할 div
 // 지도를 생성합니다    
 var map = new kakao.maps.Map(mapContainer, mapOption); 
 
-
 // 현재위치를 받아옴
 // HTML5의 geolocation으로 사용할 수 있는지 확인합니다 
-if (navigator.geolocation) {
+// if (navigator.geolocation) {
     
-    // GeoLocation을 이용해서 접속 위치를 얻어옵니다
-    navigator.geolocation.getCurrentPosition(function(position) {
+//     // GeoLocation을 이용해서 접속 위치를 얻어옵니다
+//     navigator.geolocation.getCurrentPosition(function(position) {
         
-        var lat = position.coords.latitude, // 위도
-            lon = position.coords.longitude; // 경도
+//         var lat = position.coords.latitude, // 위도
+//             lon = position.coords.longitude; // 경도
         
-        var locPosition = new kakao.maps.LatLng(lat, lon); // 마커가 표시될 위치를 geolocation으로 얻어온 좌표로 생성합니다
+//         var locPosition = new kakao.maps.LatLng(lat, lon); // 마커가 표시될 위치를 geolocation으로 얻어온 좌표로 생성합니다
         
-        // 마커와 인포윈도우를 표시합니다
-        displayMarker(locPosition);
+//         // 마커와 인포윈도우를 표시합니다
+//         displayMarker(locPosition);
             
-      });
+//       });
     
-} else { // HTML5의 GeoLocation을 사용할 수 없을때 마커 표시 위치와 인포윈도우 내용을 설정합니다
+// } else { // HTML5의 GeoLocation을 사용할 수 없을때 마커 표시 위치와 인포윈도우 내용을 설정합니다
     
-    var locPosition = new kakao.maps.LatLng(33.450701, 126.570667);
-        alert('❌ 현재위치를 찾을 수 없습니다 ❌');
-}
+//     var locPosition = new kakao.maps.LatLng(33.450701, 126.570667);
+//         alert('❌ 현재위치를 찾을 수 없습니다 ❌');
+// }
 
 // 장소 검색 객체를 생성합니다
 var ps = new kakao.maps.services.Places(map); 
